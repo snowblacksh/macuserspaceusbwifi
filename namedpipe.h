442738b3d694316f878f80541e4e49268cd2dbe4
themacuser <themacuser@5834e6ba-08ed-11df-956f-01f849d29158>
2010-01-24 23:22:33 +0000

    Initial commit

---
+++ namedpipe.h
@@ -0,0 +1,16 @@
+//
+//  namedpipe.h
+//  rtl8187tuntap
+//
+//  Created by gm on 21/01/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import <Cocoa/Cocoa.h>
+#import "USBJack.h"
+
+@interface namedpipe : NSObject {
+
+}
+- (id) namedPipe:(NSValue *)drvrptr;
+@end
---
+++ rtl8187_main.mm
@@ -0,0 +1,19 @@
+//
+//  rtl8187_main.mm
+//  rtl8187tuntap
+//
+//  Created by gm on 20/01/10.
+//  Copyright 2010 __MyCompanyName__. All rights reserved.
+//
+
+#import "rtl8187_main.h"
+
+@implementation rtl8187_main
+
+- (id) init {
+	return self;
+}
+
+
+
+@end
